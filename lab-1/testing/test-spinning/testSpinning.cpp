181e29ed2f5a9fbb91178293de8694bb39423f63
Ignacio Suárez <imsm2424@gmail.com>
2025-03-22 19:18:44 -0300

    fix: pointers to test cases and makefile

--- lab-1/testing/test-spinning/testSpinning.cpp
+++ lab-1/testing/test-spinning/testSpinning.cpp
@@ -10,37 +10,44 @@ int main()
     // ID, nombre, turno, cantBicicletas
 
     // BVA = Boundary Value Analysis
-    Spinning TC001 = Spinning(1, "TC001", Manana, 0);
-    assert(TC001.getCantBicicletas() == 0);
-    Spinning TC002 = Spinning(2, "TC002", Tarde, 1); 
-    assert(TC002.getCantBicicletas() == 1);
-    Spinning TC003 = Spinning(3, "TC003", Noche, 49);
-    assert(TC003.getCantBicicletas() == 49);
-    Spinning TC004 = Spinning(4, "TC004", Manana, 50);
-    assert(TC004.getCantBicicletas() == 50);
-    Spinning TC005 = Spinning(5, "TC005", Tarde, 51);
-    assert(TC005.getCantBicicletas() == 51);
+    Spinning* TC001 = new Spinning(1, "TC001", Manana, 0);
+    assert(TC001->getCantBicicletas() == 0);
+    Spinning* TC002 = new Spinning(2, "TC002", Tarde, 1); 
+    assert(TC002->getCantBicicletas() == 1);
+    Spinning* TC003 = new Spinning(3, "TC003", Noche, 49);
+    assert(TC003->getCantBicicletas() == 49);
+    Spinning* TC004 = new Spinning(4, "TC004", Manana, 50);
+    assert(TC004->getCantBicicletas() == 50);
+    Spinning* TC005 = new Spinning(5, "TC005", Tarde, 51);
+    assert(TC005->getCantBicicletas() == 51);
     //-----------------------------------------------------
 
-    // // Float inválidos
-    // try{
-    //     Spinning TC000 = Spinning(0.1, "TC006", Manana, 30);
-    // }catch(...){
-    //     cout << "Error esperado" << endl;
-    // }
+    // Float inválidos
+    try{
+        Spinning TC000 = Spinning(0.1, "TC006", Manana, 30);
+    }catch(...){
+        cout << "Error esperado" << endl;
+    }
 
-    // try{
-    //     Spinning TC007 = Spinning(6, "TC006", Tarde, 0.5);
-    // }catch(...){
-    //     cout << "Resultado esperado" << endl;
-    // }
+    try{
+        Spinning TC007 = Spinning(6, "TC006", Tarde, 0.5);
+    }catch(...){
+        cout << "Resultado esperado" << endl;
+    }
     
-    // try{
-    //     Spinning TC008 = Spinning(7.5, "TC006", Noche, 5.1);
-    // }catch(...){
-    //     cout << "Resultado esperado" << endl;
-    // }
+    try{
+        Spinning TC008 = Spinning(7.5, "TC006", Noche, 5.1);
+    }catch(...){
+        cout << "Resultado esperado" << endl;
+    }
     //-----------------------------------------------------
     cout << "Todos los tests pasaron correctamente." << endl;
+
+    delete TC001;
+    delete TC002;
+    delete TC003;
+    delete TC004;
+    delete TC005;
+
     return 0;
 }
