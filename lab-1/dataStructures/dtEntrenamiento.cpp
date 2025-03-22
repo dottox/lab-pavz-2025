6f93ce6f7f566145d95fe88cf13a2ffa8420d1cf
unknown <unusuariodegogle.xd@gmail.com>
2025-03-22 17:25:11 -0300

    Feat: Datatype DtEntrenamiento created

---
+++ lab-1/dataStructures/dtEntrenamiento.cpp
@@ -0,0 +1,26 @@
+#ifndef DT_ENTRENAMIENTO
+#define DT_ENTRENAMIENTO
+
+#include <iostream>
+using namespace std;
+
+class DtEntrenamiento{
+    private:
+        bool enRambla;
+    public:
+        DtEntrenamiento(){
+            this->enRambla = false;
+        };
+        DtEntrenamiento(bool b){
+            this->enRambla = b;
+        };
+        void setRambla(bool b){
+            this->enRambla = b;
+        };
+        bool getRambla(){
+            return this->enRambla;
+        };
+        ~DtEntrenamiento(){};
+};
+
+#endif
