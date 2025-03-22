3eedf3198b46e75612b8fa13fcea249f99d2b068
Ignacio Suárez <imsm2424@gmail.com>
2025-03-22 18:30:12 -0300

    feat: a lot of docs

--- lab-1/clases/Inscripcion.cpp
+++ lab-1/clases/Inscripcion.cpp
@@ -1,11 +1,29 @@
+
 #include "Inscripcion.h"
 
+/**
+ * @brief Constructor for the Inscripcion class.
+ * 
+ * Initializes an Inscripcion object with the specified date.
+ * 
+ * @param date The date of the inscription, represented as a DtFecha object.
+ */
 Inscripcion::Inscripcion(DtFecha date){
     this->fecha = date;
 };
 
+
+/**
+ * @brief Returns the date of the inscription.
+ * 
+ * @return The date of the inscription, represented as a DtFecha object.
+ */
 DtFecha Inscripcion::getFecha(){
     return this->fecha;
 };
 
+
+/**
+ * @brief Destructor for the Inscripcion class.
+ */
 Inscripcion::~Inscripcion(){};	
