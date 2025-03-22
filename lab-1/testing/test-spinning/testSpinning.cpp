c7dd06103ffc16955a12466f67e01f7121fa16f3
gyabisito <josecitohernandez5@gmail.com>
2025-03-22 20:58:59 -0300

    test: info added to spinning

--- lab-1/testing/test-spinning/testSpinning.cpp
+++ lab-1/testing/test-spinning/testSpinning.cpp
@@ -15,13 +15,16 @@ int main()
         assert(TC001->getCantBicicletas() == 0);
         delete TC001;
     } catch (...) {
-        cout << "Error inesperado en TC001" << endl;
+        cout << "Error Esperado en TC001" << endl;
     }
 
     try {
         DtSpinning dtTC002=DtSpinning(2, "TC002", Tarde,1);
         Spinning * TC002 = new Spinning(dtTC002);
         assert(TC002->getCantBicicletas() == 1);
+        assert(TC002->cupo() == 1);
+        TC002->setCantBicicletas(2);
+        assert(TC002->getCantBicicletas() == 2);
         delete TC002;
     } catch (...) {
         cout << "Error inesperado en TC002" << endl;
@@ -61,7 +64,7 @@ int main()
         Spinning * TC006 = new Spinning(dtTC006);
         delete TC006;
     }catch(...){
-        cout << "Error esperado" << endl;
+        cout << "Error esperado en TC006" << endl;
     }
 
     try{
@@ -69,7 +72,7 @@ int main()
         Spinning * TC007 = new Spinning(dtTC007);
         delete TC007;
     }catch(...){
-        cout << "Resultado esperado" << endl;
+        cout << "Resultado esperado en TC007" << endl;
     }
     
     try{
@@ -77,7 +80,7 @@ int main()
         Spinning * TC008 = new Spinning(dtTC008);
         delete TC008;
     }catch(...){
-        cout << "Resultado esperado" << endl;
+        cout << "Resultado esperado en TC008" << endl;
     }
 
     //-----------------Numeros negativos-------------------
@@ -86,7 +89,7 @@ int main()
         Spinning * TC009 = new Spinning(dtTC009);
         delete TC009;
     }catch(...){
-        cout << "Resultado esperado" << endl;
+        cout << "Resultado esperado en TC009" << endl;
     }
 
     try{
@@ -94,14 +97,14 @@ int main()
         Spinning * TC010 = new Spinning(dtTC010);
         delete TC010;
     }catch(...){
-        cout << "Resultado esperado" << endl;
+        cout << "Resultado esperado en TC010" << endl;
     }
     try{
         DtSpinning dtTC011=DtSpinning(-11, "TC011", Manana,-15);
         Spinning * TC011 = new Spinning(dtTC011);
         delete TC011;
     }catch(...){
-        cout << "Resultado esperado" << endl;
+        cout << "Resultado esperado en TC011" << endl;
     }
     //----------------Nombre Vacio-------------------
     try{
@@ -109,16 +112,16 @@ int main()
         Spinning * TC012 = new Spinning(dtTC012);
         delete TC012;
     }catch(...){
-        cout << "Resultado esperado" << endl;
+        cout << "Resultado esperado en TC012" << endl;
     }
     //-----------------Valor fuera de rango static_cast<int>(999)-------------------
     try {
         DtSpinning dtTC013=DtSpinning(13, "TC013", static_cast<Turno>(999), 10); // agarro index 999 que no existe
         Spinning *TC013 = new Spinning(dtTC013);
         delete TC013;
-        cout << "Error: Se permitió un turno invalido." << endl;
+        cout << "Error: Se permitió un turno invalido. en TC013" << endl;
     } catch (...) {
-        cout << "Resultado esperado: Turno invalido no permitido." << endl;
+        cout << "Resultado esperado: Turno invalido no permitido. en TC013" << endl;
     }
     //------------------Límites Extremos------------------
     try{
@@ -126,35 +129,35 @@ int main()
         Spinning * TC014 = new Spinning(dtTC014);
         delete TC014;
     }catch(...){
-        cout << "Resultado esperado" << endl;
+        cout << "Resultado esperado en TC014" << endl;
     }
     try{
         DtSpinning dtTC015=DtSpinning(INT_MAX, "TC015", Manana,50);
         Spinning * TC015 = new Spinning(dtTC015);
         delete TC015;
     }catch(...){
-        cout << "Resultado esperado" << endl;
+        cout << "Resultado esperado en TC015" << endl;
     }
     try{
         DtSpinning dtTC016=DtSpinning(16, "TC016", Manana,INT_MIN);
         Spinning * TC016 = new Spinning(dtTC016);
         delete TC016;
     }    catch(...){
-        cout << "Resultado esperado" << endl;
+        cout << "Resultado esperado en TC016" << endl;
     }
     try{
         DtSpinning dtTC017=DtSpinning(17, "TC017", Manana,INT_MAX);
         Spinning * TC017 = new Spinning(dtTC017);
         delete TC017;
     }catch(...){
-        cout << "Resultado esperado" << endl;
+        cout << "Resultado esperado en TC017" << endl;
     }
     try{
         DtSpinning dtTC018=DtSpinning(INT_MIN, "TC018", Manana,INT_MIN);
         Spinning * TC018 = new Spinning(dtTC018);
         delete TC018;
     }catch(...){
-        cout << "Resultado esperado" << endl;
+        cout << "Resultado esperado en TC018" << endl;
     }
 
     //------------------------------------Constructor Vacío------------------------------------
@@ -163,7 +166,7 @@ int main()
         Spinning * TC019 = new Spinning(dtTC019);
         delete TC019;
     }catch(...){
-        cout << "Resultado esperado constructor vacio" << endl;
+        cout << "Resultado esperado constructor vacio en TC019" << endl;
     }
 
     cout << "Todos los tests pasaron correctamente." << endl;
