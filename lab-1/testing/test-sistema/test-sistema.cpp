56c79097afa113129e0218049fa0508630c24ecc
unknown <unusuariodegogle.xd@gmail.com>
2025-03-25 20:55:58 -0300

    Feat: Hicimos muchas cosas XD

--- lab-1/testing/test-sistema/test-sistema.cpp
+++ lab-1/testing/test-sistema/test-sistema.cpp
@@ -2,38 +2,94 @@
 #include "../../clases/sistema.h"
 
 using namespace std;
-int main(){
+
+int main() {
     Sistema* sistema = new Sistema();
 
-    sistema->agregarSocio("1", "Juan");
-    sistema->agregarSocio("2", "Maria");
-    sistema->agregarSocio("3", "Nacho");
-    sistema->agregarSocio("4", "Pedro");
-    sistema->agregarSocio("5", "Jose");
-    sistema->agregarSocio("6", "Josefina");
-    sistema->agregarSocio("7", "Julieta");
-    sistema->agregarSocio("8", "Pepe");
-    sistema->agregarSocio("9", "Eduardo");
-    sistema->agregarSocio("10", "Wally");
-
+    // Test agregarSocio
+    
     try {
-        sistema->agregarSocio("11", "Lol");
-    } catch (const invalid_argument e) {
+        sistema->agregarSocio("1", "Juan");
+    } catch(const invalid_argument& e) {
         cout << "Error: " << e.what() << endl;
     }
 
     try {
-        sistema->agregarSocio("1", "Maria");
-    } catch (const invalid_argument e) {
+        sistema->agregarSocio("2", "Maria");
+    } catch(const invalid_argument& e) {
         cout << "Error: " << e.what() << endl;
     }
 
     try {
-        sistema->getSocio("1387");
-    } catch (const invalid_argument e) {
+        sistema->agregarSocio("3", "Nacho");
+    } catch(const invalid_argument& e) {
         cout << "Error: " << e.what() << endl;
     }
 
+    try {
+        sistema->agregarSocio("4", "Gonzalo");
+    } catch(const invalid_argument& e) {
+        cout << "Error: " << e.what() << endl;
+    }
+    // Test agregarClase with DtSpinning
+    DtSpinning clase1 = DtSpinning(1, "Spinning", Manana, 10);
+    DtEntrenamiento clase2 = DtEntrenamiento(2, "Entrenamiento", Tarde, true);
+    DtEntrenamiento clase3 = DtEntrenamiento(1, "Entrenamiento", Tarde, true);
+    DtEntrenamiento clase4 = DtEntrenamiento(3, "Entrenamiento", Tarde, true);
+    DtSpinning clase5 = DtSpinning(4, "Spinning", Manana, 10);
+    
+    try{
+        sistema->agregarClase(clase1);
+    }catch(const invalid_argument& e){
+        cout << "Error: " << e.what() << endl;
+    }
+
+    try{
+        sistema->agregarClase(clase2);
+    }catch(const invalid_argument& e){
+        cout << "Error: " << e.what() << endl;
+    }
+
+    try{
+        sistema->agregarClase(clase3);
+    }catch(const invalid_argument& e){
+        cout << "Error: " << e.what() << endl;
+    }
+
+    try{
+        sistema->agregarClase(clase4);
+    }catch(const invalid_argument& e){
+        cout << "Error: " << e.what() << endl;
+    }
+
+    try{
+        sistema->agregarClase(clase5);
+    }catch(const invalid_argument& e){
+        cout << "Error: " << e.what() << endl;
+    }
+
+    // Test agregarInscripcion
+
+    try
+    {
+        sistema->agregarInscripcion("2", 1, DtFecha(1, 1, 2021));
+    }
+    catch(const invalid_argument& e)
+    {
+        cout << "Error: " << e.what() << endl;
+    }
+
+    try
+    {
+        sistema->agregarInscripcion("1", 1, DtFecha(1, 1, 2021));
+    }
+    catch(const invalid_argument& e)
+    {
+        cout << "Error: " << e.what() << endl;
+    }
+    
+    
+    sistema->listarClases();
     delete sistema;
 
     cout << "[/////////////////////////////- Tests Passed -/////////////////////////////////]" << endl;
