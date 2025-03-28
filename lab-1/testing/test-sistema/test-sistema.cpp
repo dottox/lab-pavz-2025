0fb3322367566035c7e7729ac7ef8e7afe792fe1
Ignacio Suárez <imsm2424@gmail.com>
2025-03-28 20:58:40 -0300

    fix: test-sistemas.cpp now has good tests

--- lab-1/testing/test-sistema/test-sistema.cpp
+++ lab-1/testing/test-sistema/test-sistema.cpp
@@ -7,68 +7,75 @@ using namespace std;
 int main() {
     Sistema* sistema = new Sistema();
 
-    // Test agregarSocio
+    // --------------------- SOCIOS -----------------------------
 
+    // Agregar socios bases
+      sistema->agregarSocio("12345678", "Juan");
+      sistema->agregarSocio("12345679", "Maria");
+
+    // TC-001: Test de agregar socio con CI de otro socio
     try {
-        sistema->agregarSocio("12345678", "Juan");
+        sistema->agregarSocio("12345678", "Gonzalo");
+        throw runtime_error("Error: Se permitio agregar un socio con la misma CI que otro");
     } catch(const invalid_argument& e) {
-        cout << "Error: " << e.what() << endl;
+        cout << "TC-001 pasó correctamente." << endl;
+    } catch (const runtime_error& e) {
+        terminate();
     }
 
+    sistema->agregarSocio("12345670", "Juancho");
+
+
+    // TC-002: Test de agregar socio sobre el máximo de socios
     try {
-        sistema->agregarSocio("21345678", "Maria");
+      sistema->agregarSocio("12312312", "Gonzalo");
+      throw runtime_error("Error: Se permitio agregar un socio superando el limite máximo");
     } catch(const invalid_argument& e) {
-        cout << "Error: " << e.what() << endl;
+        cout << "TC-002 pasó correctamente." << endl;
+    } catch (const runtime_error& e) {
+        terminate();
     }
 
-    try {
-        sistema->agregarSocio("31245678", "Nacho");
-    } catch(const invalid_argument& e) {
-        cout << "Error: " << e.what() << endl;
-    }
 
-    try {
-        sistema->agregarSocio("41235678", "Gonzalo");
-    } catch(const invalid_argument& e) {
-        cout << "Error: " << e.what() << endl;
-    }
 
-    // Test agregarClase with DtSpinning and DtEntrenamiento
+    // --------------------- CLASES -----------------------------
 
+
+    // Crear clases bases
     DtSpinning clase1 = DtSpinning(1, "Spinning", Manana, 10);
     DtEntrenamiento clase2 = DtEntrenamiento(2, "Entrenamiento", Tarde, true);
-    DtEntrenamiento clase3 = DtEntrenamiento(1, "Entrenamiento", Tarde, true);
-    DtEntrenamiento clase4 = DtEntrenamiento(3, "Entrenamiento", Tarde, true);
-    DtSpinning clase5 = DtSpinning(4, "Spinning", Manana, 10);
+    DtSpinning clase3 = DtSpinning(3, "Spinning", Noche, 10);
     
-    try{
-        sistema->agregarClase(clase1);
-    }catch(const invalid_argument& e){
-        cout << "Error: " << e.what() << endl;
-    }
-
+    // Agregar clases bases
+    sistema->agregarClase(clase1);
+    sistema->agregarClase(clase2);
+  
+    // TC-003: Test de agregar una clase que ya existe
     try{
         sistema->agregarClase(clase2);
-    }catch(const invalid_argument& e){
-        cout << "Error: " << e.what() << endl;
+        throw runtime_error("Error: Se permitio agregar una clase ya existente");
+    } catch(const invalid_argument& e) {
+        cout << "TC-003 pasó correctamente" << endl;
+    } catch (const runtime_error& e) {
+        terminate();
     }
 
+    sistema->agregarClase(clase3);
+
+    // TC-004: Test de agregar una clase superando el máximo de clases
     try{
-        sistema->agregarClase(clase3);
-    }catch(const invalid_argument& e){
-        cout << "Error: " << e.what() << endl;
-    }
-    try{
+        DtSpinning clase4 = DtSpinning(4, "Spinning", Manana, 10);
         sistema->agregarClase(clase4);
-    }catch(const invalid_argument& e){
-        cout << "Error: " << e.what() << endl;
+        throw runtime_error("Error: Se permitio agregar una clase superando el limite maximo");
+    } catch(const invalid_argument& e) {
+        cout << "TC-004 pasó correctamente." << endl;
+    } catch (const runtime_error& e) {
+        terminate();
     }
     
-    try{
-        sistema->agregarClase(clase5);
-    }catch(const invalid_argument& e){
-        cout << "Error: " << e.what() << endl;
-    }
+
+    // --------------------- INSCRIPCIONES -----------------------------
+
     
     // Test agregarInscripcion
     DtFecha date = DtFecha(1, 1, 2021); // Example date
@@ -76,36 +83,41 @@ int main() {
     DtFecha date3 = DtFecha(20, 4, 2023); // Example date
     DtFecha date4 = DtFecha(10, 5, 2024); // Example date
     
+    // Agregar inscripciones bases.
+    sistema->agregarInscripcion("12345678", 1, date);
+    sistema->agregarInscripcion("12345679", 1, date2);
+    sistema->agregarInscripcion("12345670", 1, date3);
+
+    // TC-005: Agregar una inscripción con un socio inválido
     try{
-        sistema->agregarInscripcion("12345678", 1, date);
-        sistema->agregarInscripcion("21345678", 1, date2);
-        sistema->agregarInscripcion("31245678", 1, date3);
-    }catch(const invalid_argument& e){
-        cout << "Error: " << e.what() << endl;
+        sistema->agregarInscripcion("00001111", 1, date4);
+        throw runtime_error("Error: Se permitio agregar una inscripcion con un socio invalido");
+    } catch(const invalid_argument& e){
+        cout << "TC-005 pasó correctamente." << endl;
+    } catch (const runtime_error& e){
+        terminate();
     }
 
+    // TC-006: Agregar una inscripción con una clase inválida
     try{
-        sistema->agregarInscripcion("31245678", 2, date2);
-    }catch(const invalid_argument& e){
-        cout << "Error: " << e.what() << endl;
+        sistema->agregarInscripcion("12345678", 1337, date);
+        throw runtime_error("Error: Se permitio agregar una inscripcion con una clase invalida");
+    } catch(const invalid_argument& e){
+        cout << "TC-006 pasó correctamente." << endl;
+    } catch (const runtime_error& e){
+        terminate();
     }
-    
+
+    // TC-007: TODO: Añadir testeo de cupos en una clase
+
+    // TC-008: Agregar una inscripción que ya existe
     try{
-        sistema->agregarInscripcion("41235678", 1, date);
-    }catch(const invalid_argument& e){
-        cout << "Error: " << e.what() << endl;
-    }
-    
-    try{
-        sistema->agregarInscripcion("21345678", 4, date2);
-    }catch(const invalid_argument& e){
-        cout << "Error: " << e.what() << endl;
-    }
-    
-    try{
-        sistema->agregarInscripcion("41235678", 2, date2);
-    }catch(const invalid_argument& e){
-        cout << "Error: " << e.what() << endl;
+        sistema->agregarInscripcion("12345678", 1, date);
+        throw runtime_error("Error: Se permitio agregar una inscripcion que ya existe");
+    } catch(const invalid_argument& e){
+        cout << "TC-008 pasó correctamente." << endl;
+    } catch (const runtime_error& e){
+        terminate();
     }
     
     delete sistema;
