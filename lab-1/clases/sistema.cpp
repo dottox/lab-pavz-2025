7dc74b4795ff4f04e5a43982537c3538ef975b52
VisualZac <144742015+VisualZac@users.noreply.github.com>
2025-03-30 17:07:51 -0300

    FEAT: Function obtenerClase Released

--- lab-1/clases/sistema.cpp
+++ lab-1/clases/sistema.cpp
@@ -111,4 +111,12 @@ DtSocio ** Sistema::obtenerInfoSociosPorClase (int idClase,int & cantSocios){
   }
   return dtSocios;
 }
+DtClase* Sistema::obtenerClase (int idClase){
+  if(getClase(idClase) == nullptr) throw invalid_argument(ERROR_NO_CLASE_ID);
+  getClase(idClase)->getId();
+  getClase(idClase)->getNombre();
+  getClase(idClase)->getTurno();
+  DtClase * dtClase = new DtClase(getClase(idClase)->getId(), getClase(idClase)->getNombre(), getClase(idClase)->getTurno());
+  return dtClase;
+}
   
