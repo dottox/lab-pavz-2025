7680f32387c0b3cfc918842e4dd6bda208c8eda6
Ignacio Suárez <imsm2424@gmail.com>
2025-04-02 13:58:29 -0300

    fix: obtenerClase->dtClase missing cupo parameter

--- lab-1/clases/sistema.cpp
+++ lab-1/clases/sistema.cpp
@@ -114,6 +114,6 @@ DtSocio ** Sistema::obtenerInfoSociosPorClase (int idClase,int & cantSocios){
 
 DtClase* Sistema::obtenerClase (int idClase){
   if(getClase(idClase) == nullptr) throw invalid_argument(ERROR_NO_CLASE_ID);
-  DtClase * dtClase = new DtClase(getClase(idClase)->getId(), getClase(idClase)->getNombre(), getClase(idClase)->getTurno());
+  DtClase * dtClase = new DtClase(getClase(idClase)->getId(), getClase(idClase)->getNombre(), getClase(idClase)->getTurno(), getClase(idClase)->cupo());
   return dtClase;
 }
