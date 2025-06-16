52df9b6a8349c1c647d925cf45951b49e7ca6b10
Zarsu <unusuariodegogle.xd@gmail.com>
2025-06-16 18:32:04 -0300

    Fix: AltaVenta

--- lab-5/main.cpp
+++ lab-5/main.cpp
@@ -560,12 +560,12 @@ void iniciarVenta(ISistema *s)
     cin >> idEmpleado;
     cin.ignore();
 
-    if (idEmpleado.empty())
+    try
     {
-        throw invalid_argument("El ID del empleado no puede estar vacío.");
+        stoi(idEmpleado);
+        s->seleccionarMozo(stoi(idEmpleado));
     }
-
-    while (flag)
+    catch (invalid_argument &e)
     {
         cout << "Error: has ingresado un número no válido." << endl;
         s->cancelarAltaVenta(); // No debería ser necesario, pero por si acaso
