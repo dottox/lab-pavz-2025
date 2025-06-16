33482d82ed3164ccf2a2332bfc521ec0fe6fbfab
Zarsu <unusuariodegogle.xd@gmail.com>
2025-06-16 16:32:57 -0300

    Fix: fixing Merge

--- lab-5/datatypes/Producto/DtMenu.cpp
+++ lab-5/datatypes/Producto/DtMenu.cpp
@@ -19,10 +19,6 @@ ostream& operator<<(ostream& os, const DtMenu& menu)
     return os;
 }
 
-DtMenu::DtMenu(char* codigo, string descripcion, float precio) : DtProducto(codigo, descripcion, TipoMenu, precio) {
-
-}
-
 DtProducto* DtMenu::clone() const{
     return new DtMenu(*this);
 }
